09136dcd2d58137ff17054bc77d157c4f8cd3b68
hp <riteshgharti121232@gmail.com>
2025-08-28 17:35:19 +0530

    add gsc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,41 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AI YouTube Subtitle Summarizer | Instant Video Summaries",
+  title: "Free YouTube Subtitle & AI Video Summarizer",
   description:
-    "Summarize YouTube videos instantly with AI. Paste any YouTube link and get clear, concise subtitles and key insights in seconds. Free and fast subtitle summarizer powered by AI.",
-    icons: {
-      icon: "/logo.svg",  
+    "Paste a YouTube link and instantly generate subtitles with AI-powered video summarizer. Simple, fast, and free.",
+  icons: {
+    icon: "/logo.svg",
+    apple: "/logo.svg",
+  },
+  other: {
+    "google-site-verification": "79YkN7Gc3vZ8t5dvAcUZO-HrLL2v1iR7y2N6JxDzMnY",
+  },
+  openGraph: {
+    title: "AI YouTube Subtitle Summarizer",
+    description:
+      "Summarize YouTube videos and generate subtitles instantly with AI. Just paste a link and get a summary.",
+
+    url: "https://next-ai-subtitle-summarizer.vercel.app/",
+    siteName: "AI YouTube Subtitle Summarizer",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "YouTube AI Subtitle Summarizer",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AI YouTube Subtitle Summarizer",
+    description:
+      "Summarize YouTube videos instantly with AI and get subtitles. 100% free.",
+    images: ["/logo.svg"],
   },
 };
-
 
 export default function RootLayout({
   children,
